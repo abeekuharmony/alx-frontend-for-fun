b880eb0b769fed45b5c04d85f2cb2e1c9f5076d8
abeekuharmony <abeekuharmony@gmail.com>
2023-07-11 18:10:35 +0000

    file solutions

--- markdown2html.py
+++ markdown2html.py
@@ -18,11 +18,47 @@ Requirements:
 
 import sys
 import os.path
+import re
+import hashlib
 
 
 def convert_markdown_to_html(input_file, output_file):
-    # Perform the conversion here (replace with your actual code)
-    print(f"Converting {input_file} to {output_file}")
+    # Read the content of the Markdown file
+    with open(input_file, 'r') as file:
+        markdown_content = file.read()
+
+    # Convert Markdown headings to HTML
+    markdown_content = re.sub(r'^(#+)(.*)', convert_heading, markdown_content, flags=re.MULTILINE)
+
+    # Convert Markdown unordered lists to HTML
+    markdown_content = re.sub(r'^\s*-\s+(.*)', r'<li>\1</li>', markdown_content, flags=re.MULTILINE)
+    markdown_content = f'<ul>{markdown_content}</ul>'
+
+    # Convert Markdown ordered lists to HTML
+    markdown_content = re.sub(r'^\s*\*\s+(.*)', r'<li>\1</li>', markdown_content, flags=re.MULTILINE)
+    markdown_content = f'<ol>{markdown_content}</ol>'
+
+    # Convert Markdown paragraphs to HTML
+    markdown_content = re.sub(r'^([^#\-\*\n\r].*)', r'<p>\1</p>', markdown_content, flags=re.MULTILINE)
+    markdown_content = re.sub(r'\n', r'<br />\n', markdown_content)
+
+    # Convert bold syntax to HTML
+    markdown_content = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', markdown_content)
+
+    # Convert additional syntax for generating HTML
+    markdown_content = re.sub(r'\[\[(.*?)\]\]', lambda match: hashlib.md5(match.group(1).encode()).hexdigest(), markdown_content)
+    markdown_content = re.sub(r'\(\((.*?)\)\)', lambda match: match.group(1).replace('c', '').replace('C', ''), markdown_content, flags=re.IGNORECASE)
+
+    # Write the HTML content to the output file
+    with open(output_file, 'w') as file:
+        file.write(markdown_content)
+
+
+def convert_heading(match):
+    heading_level = len(match.group(1))
+    heading_text = match.group(2).strip()
+    html_heading = f"<h{heading_level}>{heading_text}</h{heading_level}>"
+    return html_heading
 
 
 if __name__ == "__main__":
